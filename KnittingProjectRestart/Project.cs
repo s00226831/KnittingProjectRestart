6da5567ae0cc29eee869dadb0809eafa1a62dd30
MeganBC <123363704+MeganBC@users.noreply.github.com>
2024-04-17 08:22:19 +0100

    Add project files.

---
+++ KnittingProjectRestart/Project.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace KnittingProjectRestart
+{
+    public class Project
+    {
+        public string ProjectName { get; set; }    
+        public string ProjectDescription { get; set; }
+        public ProjectStatus Status { get; set; }
+
+        public Project(string name, string description)
+        {
+            ProjectName = name;
+            ProjectDescription = description;
+            Status = ProjectStatus.InProgress;
+        }
+
+
+    }
+
+    // status to allow you to mark the projects you have finished/ are working on atm
+    public enum ProjectStatus
+    {
+        Complete,
+        InProgress,
+        Dropped
+    }
+
+
+}
